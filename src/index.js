3b1d4aba644c44a457865d9fa5ca1b004c4b8ee3
Andre Barros <andrebarros7201@gmail.com>
2024-05-31 20:02:02 +0100

    about me section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Test");
+import "./style.css";
+import placeholderImage from "./images/placeholder.jpg";
+
+const aboutMeLeft = document.querySelector("#left");
+const image = new Image();
+image.src = placeholderImage;
+
+aboutMeLeft.appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
-      title: "Test",
+      title: "Portfolio",
       filename: "index.html",
     }),
   ],
