a00ab87800a4aecf84d95bd3507343930ec58222
Andre Barros <andrebarros7201@gmail.com>
2024-05-31 21:49:24 +0100

    contact section

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import placeholderImage from "./images/placeholder.jpg";
 const aboutMeLeft = document.querySelector("#left");
 const image = new Image();
 image.src = placeholderImage;
-
 aboutMeLeft.appendChild(image);
+
+const contact = document.querySelector(".contact .right");
+const image2 = new Image();
+image2.src = placeholderImage;
+contact.appendChild(image2);
